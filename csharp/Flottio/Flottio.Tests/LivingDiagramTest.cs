3fa5755b65a2be06e90b5f8c97e2327bed2b273d
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-04-12 15:08:38 +0200

    Generate diagram form DotGraph object

--- csharp/Flottio/Flottio.Tests/LivingDiagramTest.cs
+++ csharp/Flottio/Flottio.Tests/LivingDiagramTest.cs
@@ -1,5 +1,6 @@
 using DotNetGraph;
 using DotNetGraph.Attributes;
+using DotNetGraph.Node;
 using DotNetGraph.SubGraph;
 using Flottio.FuelCardMonitoring.Domain;
 using NFluent;
@@ -9,6 +10,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using Xunit;
 
 namespace Flottio.Tests
@@ -35,16 +37,52 @@ namespace Flottio.Tests
             var infraSubGraph = CreateSubGraph(infraClasses, "Infra");
             graph.Elements.Add(infraSubGraph);
 
-            DotNetGraph.Compiler.DotCompiler compiler = new DotNetGraph.Compiler.DotCompiler(graph);
-            var compiledGraph = compiler.Compile();
+            string content = CreateContent(graph);
 
             string templatePath = "./Ressources/viz-template.html";
             var template = ReadTemplate(templatePath);
             var title = "Living Diagram";
-            var text = Evaluate(template, title, compiledGraph);
+            var text = Evaluate(template, title, content);
             Write("livinggdiagram.html", text);
 
-            Check.That(compiledGraph).IsEqualTo("");
+            //Check.That(compiledGraph).IsEqualTo("");
+
+        }
+
+        private string CreateContent(DotGraph graph)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"# Class diagram {graph.Identifier}");
+            stringBuilder.AppendLine("digraph G {");
+            stringBuilder.AppendLine($"graph[labelloc = top, label =\"{graph.Identifier}\", fontname = \"Verdana\", fontsize = 12, rankdir = LR];");
+            stringBuilder.AppendLine("node [fontname=\"Verdana\",fontsize=9,shape=record];");
+            int idElement = 1;
+            int idSubGraph = 0;
+            foreach (var element in graph.Elements)
+            {
+                var subgraph = element as DotSubGraph;
+                if (subgraph != null)
+                {
+                    stringBuilder.Append($"subgraph cluster_c{idSubGraph} ");
+                    stringBuilder.AppendLine("{");
+
+                    stringBuilder.AppendLine($"label = \"{subgraph.Identifier}\";");
+                    foreach (var item in subgraph.Elements)
+                    {
+                        var node = item as DotNode;
+                        if (node != null)
+                        {
+                            stringBuilder.AppendLine($"c{idElement} [label = \"{node.Identifier}\"]");
+                            idElement++;
+                        }
+                    }
+                    
+                    stringBuilder.AppendLine("}");
+                }
+                idSubGraph++;
+            }
+            stringBuilder.AppendLine("}");
+            return stringBuilder.ToString();
         }
 
         private void Write(string fileFullName, string content)
