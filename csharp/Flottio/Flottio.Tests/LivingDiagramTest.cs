13083c3248f3bd9f4f2c3d62ce6d9ad62208f043
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-04-12 16:44:01 +0200

    associate classes

--- csharp/Flottio/Flottio.Tests/LivingDiagramTest.cs
+++ csharp/Flottio/Flottio.Tests/LivingDiagramTest.cs
@@ -1,5 +1,6 @@
 using DotNetGraph;
 using DotNetGraph.Attributes;
+using DotNetGraph.Edge;
 using DotNetGraph.Node;
 using DotNetGraph.SubGraph;
 using Flottio.FuelCardMonitoring.Domain;
@@ -29,13 +30,40 @@ namespace Flottio.Tests
             var topLevelClasses = allClasses.Where(classe => classe.Namespace.Contains(prefix));
 
             string domainPrefix = ".Domain";
+            Dictionary<Type, int> elements = new Dictionary<Type, int>();
             var domainClasses = topLevelClasses.Where(classe => classe.Namespace.Contains(prefix + domainPrefix));
-            DotSubGraph domainGraph = CreateSubGraph(domainClasses, "Core Domain");
+            DotSubGraph domainGraph = CreateSubGraph(domainClasses, "Core Domain", elements);
             graph.Elements.Add(domainGraph);
 
             var infraClasses = topLevelClasses.Where(classe => classe.Namespace.Contains(prefix) && !classe.Namespace.Contains(domainPrefix));
-            var infraSubGraph = CreateSubGraph(infraClasses, "Infra");
+            var infraSubGraph = CreateSubGraph(infraClasses, "Infra", elements);
             graph.Elements.Add(infraSubGraph);
+
+            foreach (var classe in topLevelClasses)
+            {
+                var fields = classe.GetRuntimeFields();
+                foreach (var field in fields)
+                {
+                    if (topLevelClasses.Any(cl => field.FieldType == cl))
+                    {
+                        var edge = new DotEdge(elements[classe].ToString(), elements[field.FieldType].ToString());
+                        edge.ArrowHead = new DotEdgeArrowHeadAttribute(DotEdgeArrowType.Open);
+                        graph.Elements.Add(edge);
+                    }
+                }
+
+                var interfaces = classe.GetInterfaces();
+                foreach (var classInterface in interfaces)
+                {
+                    if (topLevelClasses.Any(cl => classInterface == cl))
+                    {
+                        var edge = new DotEdge(elements[classe].ToString(), elements[classInterface].ToString());
+                        edge.ArrowHead = new DotEdgeArrowHeadAttribute(DotEdgeArrowType.Normal);
+                        edge.Style = new DotEdgeStyleAttribute(DotEdgeStyle.Dashed);
+                        graph.Elements.Add(edge);
+                    }
+                }
+            }
 
             string content = CreateContent(graph);
 
@@ -56,30 +84,48 @@ namespace Flottio.Tests
             stringBuilder.AppendLine("digraph G {");
             stringBuilder.AppendLine($"graph[labelloc = top, label =\"{graph.Identifier}\", fontname = \"Verdana\", fontsize = 12, rankdir = LR];");
             stringBuilder.AppendLine("node [fontname=\"Verdana\",fontsize=9,shape=record];");
-            int idElement = 1;
-            int idSubGraph = 0;
             foreach (var element in graph.Elements)
             {
                 var subgraph = element as DotSubGraph;
                 if (subgraph != null)
                 {
-                    stringBuilder.Append($"subgraph cluster_c{idSubGraph} ");
+                    stringBuilder.Append($"subgraph cluster_c{subgraph.Identifier} ");
                     stringBuilder.AppendLine("{");
 
-                    stringBuilder.AppendLine($"label = \"{subgraph.Identifier}\";");
+                    stringBuilder.AppendLine($"label = \"{subgraph.Label.Text}\";");
                     foreach (var item in subgraph.Elements)
                     {
                         var node = item as DotNode;
                         if (node != null)
                         {
-                            stringBuilder.AppendLine($"c{idElement} [label = \"{node.Identifier}\"]");
-                            idElement++;
+                            stringBuilder.AppendLine($"{node.Identifier} [label = \"{node.Label.Text}\"]");
                         }
                     }
-                    
+
                     stringBuilder.AppendLine("}");
                 }
-                idSubGraph++;
+                var edge = element as DotEdge;
+                if (edge != null)
+                {
+                    stringBuilder.Append($"{(edge.Left as DotString).Value} -> {(edge.Right as DotString).Value} [");
+                    if (edge.ArrowHead != null)
+                    {
+                        string arrow = edge.ArrowHead.ArrowType == DotEdgeArrowType.Normal
+                            ? "onormal"
+                            : edge.ArrowHead.ArrowType.ToString().ToLower();
+                        stringBuilder.Append($"arrowhead={arrow},");
+                    }
+                    if(edge.ArrowTail != null)
+                    {
+                        stringBuilder.Append($"arrowtail={edge.ArrowTail.ArrowType.ToString().ToLower()},");
+                    }
+                    if (edge.Style != null)
+                    {
+                        stringBuilder.Append($"style={edge.Style.Style.ToString().ToLower()},");
+                    }
+
+                    stringBuilder.AppendLine("];");
+                }
             }
             stringBuilder.AppendLine("}");
             return stringBuilder.ToString();
@@ -108,14 +154,20 @@ namespace Flottio.Tests
             };
         }
 
-        private static DotSubGraph CreateSubGraph(IEnumerable<Type> classes, string subGraphIdentifier)
+        int idElement = 0;
+        private DotSubGraph CreateSubGraph(IEnumerable<Type> classes, string subGraphIdentifier, Dictionary<Type, int> elements)
         {
-            DotSubGraph subGraph = new DotSubGraph(subGraphIdentifier);
+            DotSubGraph subGraph = new DotSubGraph(idElement.ToString());
+            idElement++;
             foreach (var topLevelClass in classes)
             {
-                DotNetGraph.Core.IDotElement typeElement = new DotNetGraph.Node.DotNode(topLevelClass.Name);
+                var typeElement = new DotNode(idElement.ToString());
+                typeElement.Label = topLevelClass.Name;
                 subGraph.Elements.Add(typeElement);
+                elements.Add(topLevelClass, idElement);
+                idElement++;
             }
+
             subGraph.Label = subGraphIdentifier;
             subGraph.Style = DotSubGraphStyle.Dashed;
             subGraph.Color = new DotColorAttribute(Color.Red);
